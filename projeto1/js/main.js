788d489743d1c4e8491e822b7677f62d7771d498
santanarscs <raphaelstn@gmail.com>
2019-06-12 15:01:17 -0300

    adiciona menu mobile

--- projeto1/js/main.js
+++ projeto1/js/main.js
@@ -1,37 +1,45 @@
-$(".rslides").responsiveSlides({
-  auto: true,             // Boolean: Animate automatically, true or false
-  speed: 500,            // Integer: Speed of the transition, in milliseconds
-  timeout: 5000,          // Integer: Time between slide transitions, in milliseconds
+$('.rslides').responsiveSlides({
+	auto: true, // Boolean: Animate automatically, true or false
+	speed: 500, // Integer: Speed of the transition, in milliseconds
+	timeout: 5000 // Integer: Time between slide transitions, in milliseconds
 });
 
-$(".rslides_portfolio").responsiveSlides({
-  auto: true,             // Boolean: Animate automatically, true or false
-  speed: 500,            // Integer: Speed of the transition, in milliseconds
-  timeout: 4000,          // Integer: Time between slide transitions, in milliseconds
-  pager: true,           // Boolean: Show pager, true or false
+$('.rslides_portfolio').responsiveSlides({
+	auto: true, // Boolean: Animate automatically, true or false
+	speed: 500, // Integer: Speed of the transition, in milliseconds
+	timeout: 4000, // Integer: Time between slide transitions, in milliseconds
+	pager: true // Boolean: Show pager, true or false
 });
 
-Visibility.onVisible(function(){
-	setTimeout(function () {
-		$(".introducao h1").addClass("animated fadeInDown");
+Visibility.onVisible(function() {
+	setTimeout(function() {
+		$('.introducao h1').addClass('animated fadeInDown');
 	}, 400);
-	setTimeout(function () {
-		$(".introducao .btn").addClass("animated fadeInDown");
+	setTimeout(function() {
+		$('.introducao .btn').addClass('animated fadeInDown');
 	}, 800);
-	setTimeout(function () {
-		$(" .intro-card").addClass("animated fadeInDown");
+	setTimeout(function() {
+		$(' .intro-card').addClass('animated fadeInDown');
 	}, 1200);
-	setTimeout(function () {
-		$(".animar").addClass("animated fadeInDown");
+	setTimeout(function() {
+		$('.animar').addClass('animated fadeInDown');
 	}, 1600);
 
-	setTimeout(function () {
-		$(".introducao-interna h1").addClass("animated fadeInDown");
+	setTimeout(function() {
+		$('.introducao-interna h1').addClass('animated fadeInDown');
 	}, 400);
-	setTimeout(function () {
-		$(".introducao-interna p").addClass("animated fadeInDown");
+	setTimeout(function() {
+		$('.introducao-interna p').addClass('animated fadeInDown');
 	}, 800);
-	setTimeout(function () {
-		$(".animar-interno").addClass("animated fadeInDown");
+	setTimeout(function() {
+		$('.animar-interno').addClass('animated fadeInDown');
 	}, 1200);
+});
+
+/**
+ * animação do menu mobile
+ */
+$('.mobile-btn').click(function() {
+	$(this).toggleClass('active');
+	$('.mobile-menu').toggleClass('active');
 });
